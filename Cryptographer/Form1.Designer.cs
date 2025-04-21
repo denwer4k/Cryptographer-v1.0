7b65ef247b5c2e64ac48db6d52aacae302906c59
denwer4k <maksroll9@gmail.com>
2025-04-21 19:58:12 +0500

    initial main

---
+++ Cryptographer/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Cryptographer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 137);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(190, 20);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(58, 172);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(111, 38);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Encrypt";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(347, 172);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(111, 38);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Decrypt";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(310, 137);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(183, 20);
+			this.textBox2.TabIndex = 3;
+			this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(200, 12);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.ReadOnly = true;
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 4;
+			this.textBox3.Text = "Key";
+			this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(32, 97);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.ReadOnly = true;
+			this.textBox4.Size = new System.Drawing.Size(153, 20);
+			this.textBox4.TabIndex = 5;
+			this.textBox4.Text = "Sentence";
+			this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(324, 97);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.ReadOnly = true;
+			this.textBox5.Size = new System.Drawing.Size(153, 20);
+			this.textBox5.TabIndex = 6;
+			this.textBox5.Text = "Sentence";
+			this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(177, 38);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(153, 20);
+			this.textBox6.TabIndex = 7;
+			this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(200, 218);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.ReadOnly = true;
+			this.textBox7.Size = new System.Drawing.Size(100, 20);
+			this.textBox7.TabIndex = 8;
+			this.textBox7.Text = "Result";
+			this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(161, 253);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(183, 20);
+			this.textBox8.TabIndex = 9;
+			this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(515, 285);
+			this.Controls.Add(this.textBox8);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Cryptographer";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.TextBox textBox8;
+	}
+}
+
---
+++ Cryptographer/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cryptographer
+{
+	public partial class Form1 : Form
+	{
+		string _alphabet = "abcdefghijklmnopqrstuvwxyz.,!?+-=@: абвгдеёжзийклмнопрстуфхцчшщъыьэюя1234567890";
+
+		string _mainKey;
+
+		string _textForEncryption;
+		string _textForDecryption;
+
+		string _finalResult;
+
+		private string Encryption(string _currText, string _currKey)
+		{
+			
+			_currText = _currText.ToLower();
+
+			char[] _charsChange = _currText.ToCharArray();
+
+			for (int i = 0; i < _currText.Length; i++)
+			{
+				int _fNum = _alphabet.IndexOf(_charsChange[i]) + 1;
+				int _sNum = _alphabet.IndexOf(_currKey[i % _currKey.Length]) + 1;
+				int _preNum = ((_fNum + _sNum) % _alphabet.Length);
+				if(_preNum == 0)
+				{
+					_preNum = _alphabet.Length;
+				}
+
+				_charsChange[i] = _alphabet[_preNum - 1];
+			}
+
+			string _output = new string(_charsChange);
+
+			return _output;
+		}
+
+		private string Decryption(string _currText, string _currKey)
+		{
+			_currText = _currText.ToLower();
+
+			char[] _charsChange = _currText.ToCharArray();
+
+			for (int i = 0; i < _currText.Length; i++)
+			{
+				int _fNum = _alphabet.IndexOf(_charsChange[i]) + 1;
+				int _sNum = _alphabet.IndexOf(_currKey[i % _currKey.Length]) + 1;
+				int _preNum;
+				if(_fNum - _sNum > 0)
+				{
+					_preNum = _fNum - _sNum;
+				}
+				else if(_fNum - _sNum == 0)
+				{
+					_preNum = _alphabet.Length;
+				}
+				else
+				{
+					_preNum = _alphabet.Length + _fNum - _sNum;
+				}
+
+					//_preNum = (_alphabet.Length + _fNum - _sNum) % (_alphabet.Length);
+
+					_charsChange[i] = _alphabet[_preNum - 1];
+			}
+
+			string _output = new string(_charsChange);
+
+			return _output;
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			_textForEncryption = textBox1.Text;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			_finalResult = Encryption(_textForEncryption, _mainKey);
+			textBox8.Text = _finalResult;
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+			_textForDecryption = textBox2.Text;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			_finalResult = Decryption(_textForDecryption, _mainKey);
+			textBox8.Text = _finalResult;
+		}
+
+		private void textBox3_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox4_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox5_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox6_TextChanged(object sender, EventArgs e)
+		{
+			_mainKey = textBox6.Text;
+		}
+
+		private void textBox8_TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void textBox7_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
